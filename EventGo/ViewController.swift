618b6a3cf606871acbcfc1bce6220a5922721881
Nicholas Swift <nicholas.w.swift@gmail.com>
2016-06-25 16:48:23 -0700

    updated login page
    updated login page

--- EventGo/ViewController.swift
+++ EventGo/ViewController.swift
@@ -44,6 +44,7 @@ class ViewController: UIViewController {
     
     /*
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
         let eventPageController = segue.destinationViewController as! EventPageController
         if segue.identifier == "EventPage"{
             eventPageController.eventList = eventArr
