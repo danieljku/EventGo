6c0c19f1a62b40f78ca6b73465cdd0eb8fc13db2
danieljku <DKu@Daniels-MacBook-Air.local>
2016-06-25 15:23:19 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	EventGo/Base.lproj/Main.storyboard

---
+++ EventGo/SignInViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SignInViewController.swift
+//  EventGo
+//
+//  Created by Nicholas Swift on 6/25/16.
+//  Copyright © 2016 djku. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    var username: String?
+    var password: String?
+    
+}
