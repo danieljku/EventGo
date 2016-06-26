ad8734b9dfa6548feaf983ae66c472a1f626483f
Nicholas Swift <nicholas.w.swift@gmail.com>
2016-06-25 18:41:06 -0700

    added helper folder and ServerHelper

--- EventGo/SignInViewController.swift
+++ EventGo/SignInViewController.swift
@@ -47,43 +47,6 @@ class SignInViewController: UIViewController {
     }
     
     let apiURL = NSURL(string: "http://curtastic.com/eventtogo/?action=getevents")!
-    
-    /*let request = NSURLRequest(URL: apiURL)
-    
-    
-    // Create a task
-    let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-        (data, reponse, error) in
-        
-        if error == nil {
-            
-            guard let data = data else {
-                
-                print("No data was returned by the request!")
-                return
-            }
-            let parsedResult: AnyObject!
-            
-            do {
-                // Serialize means converting object to streams of bytes
-                print(data)
-                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
-                
-                print(parsedResult)
-            } catch {
-                print("Coult not parse the data as JSON: '\(data)'")
-                return
-            }
-            if let dictionary = parsedResult["events"] {
-                print(dictionary)
-                
-                for each in dictionary as! [[String: String]] {
-                    print(each["userid"]!)
-                }
-            }
-        }
-    }*/
-    
 }
 
 extension UIView {
---
+++ ServerHelper.swift
@@ -0,0 +1,47 @@
+//
+//  ServerHelper.swift
+//  EventGo
+//
+//  Created by Nicholas Swift on 6/25/16.
+//  Copyright © 2016 djku. All rights reserved.
+//
+
+import Foundation
+
+class ServerHelper {
+    /*let request = NSURLRequest(URL: apiURL)
+     
+     
+     // Create a task
+     let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+     (data, reponse, error) in
+     
+     if error == nil {
+     
+     guard let data = data else {
+     
+     print("No data was returned by the request!")
+     return
+     }
+     let parsedResult: AnyObject!
+     
+     do {
+     // Serialize means converting object to streams of bytes
+     print(data)
+     parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+     
+     print(parsedResult)
+     } catch {
+     print("Coult not parse the data as JSON: '\(data)'")
+     return
+     }
+     if let dictionary = parsedResult["events"] {
+     print(dictionary)
+     
+     for each in dictionary as! [[String: String]] {
+     print(each["userid"]!)
+     }
+     }
+     }
+     }*/
+}
